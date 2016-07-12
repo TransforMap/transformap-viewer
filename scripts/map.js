3e4a4bf87482774c7071cb7e2d1b3c0b5eb22679
keikreutler <kei@ourmachine.net>
2016-07-12 18:39:53 +0200

    Commented code

--- scripts/map.js
+++ scripts/map.js
@@ -4,7 +4,7 @@ var theme_colors = ["#fcec74", "#f7df05", "#f2bd0b", "#fff030", "#95D5D2", "#1F3
 var MapModel = Backbone.Model.extend({});
 
 var MapData = Backbone.Collection.extend({
-    url:"http://transformap.co/json/sample-data.json", // //data.transformap.co/raw/5d6b9d3d32097fd68322008744001eec",
+    url:"http://transformap.co/json/sample-data.json",
     parse: function(response){
         return response.features;
     },
@@ -13,6 +13,7 @@ var MapData = Backbone.Collection.extend({
     }
  });
 
+/* Creates map and popup template */
 var MapView = Backbone.View.extend({
     el: '#map-template',
     template: _.template($('#mapTemplate').html()),
@@ -50,10 +51,12 @@ var MapView = Backbone.View.extend({
     },
 });
 
+/* Initialises map */
 var mapData = new MapData();
 var mapView = new MapView({ collection: mapData });
 var baseTaxonomyUrl = 'https://base.transformap.co/entity/';
 
+// Collects taxonomy data
 var FilterData = Backbone.Collection.extend({
     url:"http://transformap.co/json/susy-taxonomy.json",
     parse: function(response){
@@ -88,11 +91,12 @@ var MenuView = Backbone.View.extend({
         "click .subcategory": "displayTypes"
     },
     clicked: function(e){
+        // Shows subcategories
         e.preventDefault();
-       // $("#map-menu").find(".type-of-initiative").hide();
         $(e.currentTarget).parent().find('ul.subcategories').slideToggle(400);
     },
     displayTypes: function(e){
+        // Hides previously open types of initiatives
         e.preventDefault();
         $( "ul.type-of-initiative" ).each(function() {
           $( this ).hide( );
@@ -100,11 +104,13 @@ var MenuView = Backbone.View.extend({
         $( ".subcategory" ).each(function() {
           $( this ).removeClass("active-filter");
         });
+        // Shows type of intiatives below this subcategory
         $(e.currentTarget).addClass("active-filter");
         $(e.currentTarget).find('ul.type-of-initiative').slideToggle(400).addClass("active-filter");
     },
     renderItem: function(model) {
         var filter = model.toJSON();
+        // Finds categories
         if(filter.subclass_of) {
             if(filter.subclass_of.value == 'https://base.transformap.co/entity/Q1234#SSEDAS_TAX_UUID') {
                 var str = filter.item.value;
@@ -115,6 +121,7 @@ var MenuView = Backbone.View.extend({
                 this.$el.append(this.template(filter));
             }
         }
+        // Finds categories' subcategories
         for(i = 0; i < categories.length; i++) {
             if(filter.subclass_of.value == baseTaxonomyUrl + categories[i]) {
                 var catId = '#' + categories[i];
@@ -125,77 +132,21 @@ var MenuView = Backbone.View.extend({
                 $(catId).append('<li class="subcategory list-group-item"><span class="toggle-subcategories">' + filter.itemLabel.value + '</span><ul id="' + subcategory + '" class="type-of-initiative"></ul></li>');
             }
         }
-        for(i = 0; i < subcategories.length; i++) {
-            var catId = '#' + subcategories[i];
-            if(filter.subclass_of.value == baseTaxonomyUrl + subcategories[i]) {
-                var str = filter.item.value;
-                var typeOfInitiative = str.split('/');
-                typeOfInitiative = typeOfInitiative[typeOfInitiative.length - 1];
-                $(catId).append('<li class="list-group-item">' + filter.itemLabel.value + '</li>');
-            }
-            else{
-                //$(catId).remove();
-            }
-        }
-    }
-});
-/*
-var SubmenuView = Backbone.View.extend({
-    el: '#map-submenu',
-    template: _.template($('#mapSubmenuTemplate').html()),
-    initialize: function(){
-        this.listenTo(this.collection,"add", this.renderItem);
-        this.collection.fetch();   
-    },
-    render: function () {
-    },
-    events: {
-        "click .category": "clicked",
-        "click .subcategory": "clickedLevel"
-    },
-    clicked: function(e){
-        e.preventDefault();
-        $(e.currentTarget).find('ul.subcategories').show(400);
-    },
-    clickedLevel: function(e){
-        e.preventDefault();
-        $(e.currentTarget).find('ul.type-of-initiative').slideToggle(400);
-    },
-    renderItem: function(model) {
-        var filter = model.toJSON();
-        if(filter.subclass_of) {
-            if(filter.subclass_of.value == 'https://base.transformap.co/entity/Q1234#SSEDAS_TAX_UUID') {
-                var str = filter.item.value;
-                var category = str.split('/');
-                category = category[category.length - 1].split('#');
-                categories.push(category[0]);
-                filter.id = category[0];
-                this.$el.append(this.template(filter));
-            }
-        }
-        for(i = 0; i < categories.length; i++) {
-            if(filter.subclass_of.value == baseTaxonomyUrl + categories[i]) {
-                var catId = '#' + categories[i];
-                var str = filter.item.value;
-                var subcategory = str.split('/');
-                subcategory = subcategory[subcategory.length - 1];
-                subcategories.push(subcategory);
-                $(catId).append('<li class="subcategory list-group-item">' + filter.itemLabel.value + '<ul id="' + subcategory + '" class="type-of-initiative"></ul></li>');
-            }
-        }
+        // Finds subcategories' types of initiatives
         for(i = 0; i < subcategories.length; i++) {
             if(filter.subclass_of.value == baseTaxonomyUrl + subcategories[i]) {
                 var catId = '#' + subcategories[i];
                 var str = filter.item.value;
                 var typeOfInitiative = str.split('/');
                 typeOfInitiative = typeOfInitiative[typeOfInitiative.length - 1];
-                subcategories.push(typeOfInitiative);
-                $(catId).append('<li>' + filter.itemLabel.value + '</li>');
+                $(catId).append('<li class="list-group-item">' + filter.itemLabel.value + '</li>');
             }
         }
     }
-});*/
+});
 
+// Initializes templates and fetches data
 var filterData = new FilterData();
 var filterView = new MenuView({ collection: filterData });
 filterView.render();
+
