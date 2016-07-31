665c7909353a1bff27d4a73bb243f338f53a128e
Michael Maier <Michael.Maier@student.tugraz.at>
2016-07-31 23:51:36 +0200

    rewrite code to support multiple instances of elements in tax structure

--- scripts/map.js
+++ scripts/map.js
@@ -94,7 +94,7 @@ function convert_tax_to_tree() {
   }
 
   // get all child categories for the main categories
-  treejson.elements.forEach(function(category_item){ //iterate over categories
+  treejson.elements.forEach(function(category_item){ //iterate over categories to attach subcats
 
     var uuid_of_category = category_item.UUID;
     //console.log(uuid_of_category);
@@ -103,7 +103,7 @@ function convert_tax_to_tree() {
       var entry = tax_hashtable.cat_qindex[i];
 
       if(entry["subclass_of"]) {
-        if(entry["subclass_of"].value == uuid_of_category) {
+        if(-1 < $.inArray( uuid_of_category, entry["subclass_of"].value.split(";") ) ) {
           //console.log("add subcat " + entry.itemLabel.value + " to category " + category_item.itemLabel );
           var new_subcat =
             {
@@ -128,19 +128,21 @@ function convert_tax_to_tree() {
   for( i in tax_hashtable.toi_qindex) {
     var flat_type_of_initiative = tax_hashtable.toi_qindex[i];
 
-    var parent_uuid = flat_type_of_initiative.subclass_of.value;
+    var parent_uuids = flat_type_of_initiative.subclass_of.value.split(";");
 
-    cats_that_hold_type_of_initiatives.forEach(function(cat){
-      if(cat.UUID == parent_uuid) {
-        var type_of_initiative =
-          {
-            "type": "type_of_initiative",
-            "UUID": flat_type_of_initiative.item.value,
-            "itemLabel": flat_type_of_initiative.itemLabel.value,
-            "type_of_initiative_tag": flat_type_of_initiative.type_of_initiative_tag.value
-          }
-        cat.elements.push(type_of_initiative);
-      }
+    parent_uuids.forEach(function(single_toi_uuid) { //they may be subclass of more than one cat
+      cats_that_hold_type_of_initiatives.forEach(function(cat){
+        if(cat.UUID == single_toi_uuid) {
+          var type_of_initiative =
+            {
+              "type": "type_of_initiative",
+              "UUID": flat_type_of_initiative.item.value,
+              "itemLabel": flat_type_of_initiative.itemLabel.value,
+              "type_of_initiative_tag": flat_type_of_initiative.type_of_initiative_tag.value
+            }
+          cat.elements.push(type_of_initiative);
+        }
+      });
     });
   };
 
@@ -183,10 +185,10 @@ function buildTreeMenu(tree_json) {
     if(cat.elements.length != 0) {
       cat.elements.forEach(function(entry) {
         if(entry.type == "subcategory") {
-          appendCategory(entry, $('#' + cat_data.id + ' > ul.subcategories'));
+          appendCategory(entry, $('#map-menu .' + cat_data.id + ' > ul.subcategories'));
         }
         if(entry.type == "type_of_initiative") {
-          appendTypeOfInitiative(entry, $('#' + cat_data.id + ' > ul.type-of-initiative'));
+          appendTypeOfInitiative(entry, $('#map-menu .' + cat_data.id + ' > ul.type-of-initiative'));
         }
       });
     }
@@ -207,21 +209,21 @@ function clickOnCat(id) {
   console.log("clickOnCat: "+ id);
 
   //close all other cats on the same level
-  $("#" + id).parent("ul").children("li").children("ul").hide();
+  $("#map-menu ." + id).parent("ul").children("li").children("ul").hide();
   $("ul.type-of-initiative").hide();
 
   //toggle "selected" on the current level
-  $("#" + id).parent("ul").children("li").children("span").removeClass("selected");
+  $("#map-menu ." + id).parent("ul").children("li").children("span").removeClass("selected");
 
   //remove all "selected" on all child levels
-  $("#" + id + " .selected").removeClass("selected");
+  $("#map-menu ." + id + " .selected").removeClass("selected");
 
   //remove "selected" on the parent level
-  $("#" + id).parent("ul").parent("li").children("span").removeClass("selected");
+  $("#map-menu ." + id).parent("ul").parent("li").children("span").removeClass("selected");
 
-  $("#" + id + " > span").addClass("selected");
+  $("#map-menu ." + id + " > span").addClass("selected");
 
-  $("#" + id + " > ul").show();
+  $("#map-menu ." + id + " > ul").show();
   trigger_Filter(id);
 
   updateToiEmptyStatusInFilterMenu();
@@ -238,8 +240,8 @@ function resetFilter () {
 
 function clickOnInitiative(id) {
   console.log("clickOnInitiative: "+ id);
-  $("#" + id).parent("ul").children("li").removeClass("selected");
-  $("#" + id).addClass("selected");
+  $("#map-menu ." + id).parent("ul").children("li").removeClass("selected");
+  $("#map-menu ." + id).addClass("selected");
   trigger_Filter(id);
 }
 
@@ -285,7 +287,13 @@ function createToiArray(toi_string) {
   return toi_array;
 }
 
-function filterMatches(attributes,filter_UUID) {
+/**
+  * attributes:  ;-separated toi-attributes of POI
+  * filter_UUID: Q-Nr of filter that should be matched against
+  *
+  * checks if any attribute in attributes (=TOI) is either the filter_UUID or the TOI is any subclass of the filter_UUID
+  */
+function filterMatches(attributes, filter_UUID) {
   if(!attributes) {
     console.log("error in filter, no attributes");
     return false;
@@ -315,11 +323,14 @@ function filterMatches(attributes,filter_UUID) {
     var parent_cat_qnr = false;
     var to_check_qnr = type_of_initiative_QNR;
     while(tax_hashtable.all_qindex[to_check_qnr] && tax_hashtable.all_qindex[to_check_qnr].subclass_of) {
-      parent_cat_qnr = getQNR(tax_hashtable.all_qindex[to_check_qnr].subclass_of.value);
+      var parent_cat_qnrs = tax_hashtable.all_qindex[to_check_qnr].subclass_of.value.split(";");
 
-      if(parent_cat_qnr == filter_UUID)
-        return true;
-      to_check_qnr = parent_cat_qnr;
+      for(var j=0; j < parent_cat_qnrs.length; j++) {
+        parent_cat_qnr = getQNR(parent_cat_qnrs[j]);
+        if(parent_cat_qnr == filter_UUID)
+          return true;
+        to_check_qnr = parent_cat_qnr;
+      }
     }
 
   }
@@ -342,13 +353,27 @@ function fill_tax_hashtable() {
 
     var qnr = getQNR(entry.item.value);
 
-    if (entry["instance_of"].value == "https://base.transformap.co/entity/Q6") {
+    if (entry["instance_of"].value == "https://base.transformap.co/entity/Q6") { //type of initiative
       tax_hashtable.toistr_to_qnr[entry.type_of_initiative_tag.value] = qnr;
       tax_hashtable.qnr_to_toistr[qnr] = entry.type_of_initiative_tag.value;
-      tax_hashtable.toi_qindex[qnr] = entry;
-      tax_hashtable.all_qindex[qnr] = entry;
+      if(!tax_hashtable.toi_qindex[qnr]) {
+        tax_hashtable.toi_qindex[qnr] = entry;
+        tax_hashtable.all_qindex[qnr] = entry;
+      } else { // being a member of multiple categories is represented by multiple identical objects being subclass of different categories
+        if(-1 == $.inArray( entry.subclass_of.value, tax_hashtable.toi_qindex[qnr].subclass_of.value.split(";") ) ) { //just a precaution to have no dups
 
-    } else if (entry["instance_of"].value == "https://base.transformap.co/entity/Q5") {
+          //if it is the original, make a deep copy we can alter
+          if( ! tax_hashtable.toi_qindex[qnr].subclass_of.value.match(/;/)) {
+            var copy = JSON.parse(JSON.stringify(tax_hashtable.toi_qindex[qnr]));
+            tax_hashtable.toi_qindex[qnr] = copy;
+            tax_hashtable.all_qindex[qnr] = copy;
+          }
+
+          tax_hashtable.all_qindex[qnr].subclass_of.value += ";" + entry.subclass_of.value;
+        }
+      }
+
+    } else if (entry["instance_of"].value == "https://base.transformap.co/entity/Q5") { //category
       tax_hashtable.all_qindex[qnr] = entry;
       tax_hashtable.cat_qindex[qnr] = entry;
 
@@ -373,7 +398,7 @@ function updateToiEmptyStatusInFilterMenu () {
         tax_hashtable.toi_count[e]=1;
         var qnr = tax_hashtable.toistr_to_qnr[e];
         if(qnr) // only works lateron when menu is loaded
-          $('#' + qnr).removeClass('empty');
+          $('#map-menu .' + qnr).removeClass('empty');
       } else {
         tax_hashtable.toi_count[e]++
       }
