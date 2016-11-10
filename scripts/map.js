4a2b8d788d146e1dc4b41f391a55ce9cfc43500b
Michael Maier <species@sbox.tugraz.at>
2016-11-10 15:44:08 +0100

    improvements to bg layer definitions

--- scripts/map.js
+++ scripts/map.js
@@ -48,41 +48,48 @@ var map,
     center,
     zoom,
     defaultlayer,
-    base_maps,
+    base_maps = {},
+    leaflet_bg_maps,
     pruneClusterLayer;
 function initMap() {
   var attr_osm = 'Map data by <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, under <a href="https://www.openstreetmap.org/copyright">ODbL</a>. ',
-      attr_pois = 'POIs by <a href="http://solidariteconomy.eu">SUSY</a>, <a href="https://creativecommons.org/publicdomain/zero/1.0/">CC-0</a>. '
+      attr_pois = 'POIs by <a href="http://solidariteconomy.eu">SUSY</a>, <a href="https://creativecommons.org/publicdomain/zero/1.0/">CC-0</a>. ';
 
-  osm = new L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  base_maps['mapnik'] = new L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: attr_osm + attr_pois,
       maxZoom : 19,
       noWrap: true
   });
-  terrain = new L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
+  base_maps['stamen_terrain'] = new L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
       attribution: 'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, '+
         'under <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. '+
-        attr_osm  + attr_pois
+        attr_osm  + attr_pois,
+      maxZoom : 18,
+      noWrap: true
   });
-  terrain_bg = new L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.png', {
+  base_maps['stamen_terrain_bg'] = new L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.png', {
       attribution: 'Map tiles by <a href="http://stamen.com/">Stamen Design</a>, '+
         'under <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. '+
-        attr_osm + attr_pois
+        attr_osm + attr_pois,
+      maxZoom : 18,
+      noWrap: true
   });
-  hot = new L.tileLayer('http://tile-{s}.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+  base_maps['hot'] = new L.tileLayer('http://tile-{s}.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution: 'Tiles courtesy of <a href="http://hot.openstreetmap.org/">Humanitarian OpenStreetMap Team</a>. '+
-        attr_osm + attr_pois
+        attr_osm + attr_pois,
+      maxZoom : 20,
+      noWrap: true
   });
 
-  if(!base_maps)
-    base_maps = {
-      'Stamen - Terrain': terrain,
-      'Stamen - Terrain Background': terrain_bg,
-      'OpenStreetMap - Mapnik': osm,
-      'Humanitarian OpenStreetMap ': hot
+  if(!leaflet_bg_maps)
+    leaflet_bg_maps = {
+      'Stamen - Terrain': base_maps['stamen_terrain'],
+      'Stamen - Terrain Background': base_maps['stamen_terrain_bg'],
+      'OpenStreetMap - Mapnik': base_maps['mapnik'],
+      'Humanitarian OpenStreetMap ': base_maps['hot']
     };
   if(!defaultlayer)
-    defaultlayer = terrain_bg;
+    defaultlayer = base_maps['stamen_terrain_bg'];
 
   if(!center)
     center = new L.LatLng(51.1657, 10.4515);
@@ -113,7 +120,7 @@ function initMap() {
 
   map.addLayer(pruneClusterLayer);
 
-  var ctrl = new L.Control.Layers(base_maps);
+  var ctrl = new L.Control.Layers(leaflet_bg_maps);
   map.addControl(ctrl);
 
   var hash = new L.Hash(map); // Leaflet persistent Url Hash function
